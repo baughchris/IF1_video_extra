adca64fb120a5f068fd080bbf2d867ad5156fb69
baughchris <baugh.c@nulondon.ac.uk>
2025-04-10 14:12:58 +0100

    Passes Test BuzzFizz

---
+++ .github/workflows/test_buzz_fizz.py
@@ -0,0 +1,23 @@
+#Establishing Range
+for i in range(1,101):
+    print (i)
+
+    #Creating Fizz Buzz Criteria
+#Fizz Buzz
+
+for i in range(1, 101):
+    #FizzBuzz
+    if i % 5 == 0 and i % 3 == 0: 
+        print("FizzBuzz")
+
+    #Fizz
+    elif i % 3 == 0:
+        print("Fizz")
+
+    #Buzz
+    elif i % 5 == 0:
+        print("Buzz")
+
+    #None Of The Above
+    else:
+        print(i)
